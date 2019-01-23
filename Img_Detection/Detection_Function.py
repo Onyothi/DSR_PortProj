14e75d319b6c2c03e7ae5e1612a953a3a651b419
Wilhelmina Ndapewa Nekoto <wilhelmina0711@gmail.com>
2019-01-23 20:27:48 +0200

    Code returns TypeError: wrong type

--- Img_Detection/Detection_Function.py
+++ Img_Detection/Detection_Function.py
@@ -1,39 +1,54 @@
+"""
+def detect(imagepath):
+    return [
+        {'object': 'person', 'proba': 0.98,
+         'x0': 12, 'y0': 8, 'x1': 20, 'y1': 12},
+        {'object': 'umbrella', 'proba': 0.8,
+         'x0': 57, 'y0': 40, 'x1': 80, 'y1': 42},
+    ]
 
-import os
-import cv2
+def draw_boxes(imagepath, detections):
+    return image
+
+image = draw_boxes('myimage.png', detect('myimage.png'))
+
+
+Convert to dictionary, how do the coord work, check openCV, how the function works
+"""
 import darknet
-import json
-import joblib
-from pathlib import Path
-
-# Seems to work only on txt documents - try on images
-img_folder = Path("Data_Collection/")
-file_to_open = img_folder / "capture_20181126_181828.jpg"
-f = open(file_to_open)
-print(f)
-
-# Declare network
-darknet.set_gpu(0)
-net = darknet.load_net(b"cfg/yolov3.cfg", b"weights/yolov3.weights", 0)
-meta = darknet.load_meta(b"cfg/coco.data")
 
 # Detection Function
-def detect(imagefile):
-    #filehandle = open(imagefile)
-    #print(filehandle.read())
-    #filehandle.close()
-    r = darknet.detect(net, meta, imagefile.encode())
+def detect(net, meta, imagepath):
+    r = darknet.detect(net, meta, imagepath)
     return r
-    #print("image is:" + file + ".")
-    #print("path is:" + file + ".")
 
-# Calling detect function from the main method
+
+def test_detect_data_structure(net, meta):
+    result = detect(net, meta, "test_img/test_image1.jpg")
+    assert isinstance(result, list)
+    assert len(result) != 0
+    for entry in result:
+        print(entry)
+
+        assert sorted(entry.keys()) == [
+            'object',  # string such as 'umbrella'
+            'proba',  # proba between 0 and 1
+            'x0', 'y0',  # lower left corner
+            'x1', 'y1'  # upper right corner
+            ]
+        assert isinstance(entry['object'], str)
+        assert isinstance(entry['proba'], float)
+        assert isinstance(entry['x0'], int)
+
 
 def main():
-    #detect(capture_20181126_181828.jpg)
-    #fileDir = os.path.dirname(os.path.realpath('__file__'))
-    #print(fileDir)
-    imagefile = "capture_20181126_181828.jpg"
-    detect(imagefile)
-    result_list.append(r)
-    print(r)
+    # Declare network
+    darknet.set_gpu(0)
+    net = darknet.load_net(b"cfg/yolov3.cfg", "weights/yolov3.weights", 0)
+    meta = darknet.load_meta(b"cfg/coco.data")
+
+    test_detect_data_structure(net, meta)
+
+
+if __name__ == '__main__':
+    main()
